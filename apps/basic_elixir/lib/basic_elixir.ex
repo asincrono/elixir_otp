7aef9f67b41e4226c27812b8264094b11a0cc8f2
Manuel C. Piñeiro Mourazos <asincrono@gmail.com>
2018-01-26 22:23:32 +0100

    First steps: sequential client

--- apps/basic_elixir/lib/basic_elixir.ex
+++ apps/basic_elixir/lib/basic_elixir.ex
@@ -15,4 +15,50 @@ defmodule BasicElixir do
   def hello do
     :world
   end
+
+  @doc """
+  Adds a list of numbers together.
+  """
+  def sum([]), do: 0
+
+  def sum([h | t]) when is_number(h) do
+    h + sum(t)
+  end
+
+  @doc """
+  Another way to sum.
+  """
+  def sum2(l) when is_list(l), do: do_sum2(0, l)
+
+  defp do_sum2(acc, []), do: acc
+
+  defp do_sum2(acc, [h | t]) when is_number(h) do
+    do_sum2(acc + h, t)
+  end
+
+  def sum3(l) when is_list(l) do
+    # Enum.reduce(l, &(&1 + &2))
+    Enum.reduce(l, &+/2)
+  end
+
+  @doc """
+  Transmforms a list of numbers into its squared reversed version.
+  """
+  def transform(l) when is_list(l), do: do_transform([], l)
+
+  def do_transform(l_out, []), do: l_out
+
+  def do_transform(l_out, [h | t]) do
+    do_transform([h * h | l_out], t)
+  end
+
+  def transform2(l) when is_list(l) do
+    Enum.map(Enum.reverse(l), &(&1 * &1))
+  end
+
+  def transform3(l) when is_list(l) do
+    l
+    |> Enum.reverse()
+    |> Enum.map(&(&1 * &1))
+  end
 end
---
+++ apps/metex/lib/metex/worker.ex
@@ -0,0 +1,111 @@
+defmodule Metex.Worker do
+  @moduledoc """
+  Worker to fetch data from the web.
+  """
+
+  @doc """
+  Returns info from the API.
+  """
+  def get_info(address) do
+    info =
+      address
+      |> get_location
+      |> url_for_location
+      |> Enum.map(&HTTPoison.get(&1))
+      |> Enum.map(&parse_info(&1))
+  end
+
+  @doc """
+  We generate an URL from the location info.
+  """
+  def url_for_location(locations) do
+    now = NaiveDateTime.utc_now()
+
+    start_time =
+      now
+      |> NaiveDateTime.add(-1800, :second)
+      |> NaiveDateTime.truncate(:second)
+      |> NaiveDateTime.to_iso8601()
+
+    IO.puts(start_time)
+
+    end_time =
+      now
+      |> NaiveDateTime.add(1800, :second)
+      |> NaiveDateTime.truncate(:second)
+      |> NaiveDateTime.to_iso8601()
+
+    IO.puts(end_time)
+
+    Enum.map(locations, fn location ->
+      %{lng: lng, lat: lat} = location
+
+      "http://servizos.meteogalicia.es/apiv3/getNumericForecastInfo?coords=#{lng},#{lat}&variables=temperature&startTime=#{
+        start_time
+      }&endTime=#{end_time}&API_KEY=#{get_api_key(:meteosix)}"
+    end)
+  end
+
+  @doc """
+  Return the API url from an address
+  """
+  def url_for_latitude(address) do
+    p_address =
+      String.split(address)
+      |> Enum.join("+")
+
+    "https://maps.googleapis.com/maps/api/geocode/json?address=#{p_address}&key=#{
+      get_api_key(:google_maps_geocoding)
+    }"
+  end
+
+  @doc """
+  Get the coordinates from Google Maps Geocoding.
+  """
+  def get_location(address) when is_binary(address) do
+    %{"results" => results} =
+      HTTPoison.get(url_for_latitude(address))
+      |> parse_response
+
+    Enum.map(results, fn location ->
+      %{
+        # "geometry" => %{"lng" => lng, "lat" => lat},
+        "formatted_address" => address,
+        "geometry" => %{"location" => %{"lng" => lng, "lat" => lat}}
+      } = location
+
+      # %{address: address, lng: lng, lat: lat}
+      %{address: address, lng: lng, lat: lat}
+    end)
+
+    # %{"geomerty" => %{"location" => %{"lat" => lat, "lng" => lng}}} = body
+    # %{lng: lng, lat: lat}
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body}}) do
+    JSON.decode!(body)
+  end
+
+  defp parse_info({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    %{"features" => features} = JSON.decode!(body)
+
+    features
+    |> Enum.map(fn feature ->
+      %{"properties" => %{"days" => days}} = feature
+      List.first(days)
+    end)
+    |> Enum.map(fn day ->
+      %{"variables" => variables} = day
+      List.first(variables)
+    end)
+    |> Enum.map(fn variable ->
+      IO.inspect(variable)
+      %{"name" => name, "values" => values} = variable
+      %{"value" => value} = List.first(values)
+      %{name: name, value: value}
+    end)
+  end
+
+  defp get_api_key(:meteosix), do: System.get_env("METEOGALICIA_API_KEY")
+  defp get_api_key(:google_maps_geocoding), do: System.get_env("GMG_API_KEY")
+end
